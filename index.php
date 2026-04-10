5faa28cbc4759cd95128a8c2c81feb076662a16d
Julia <julia.04910@aluno.iffar.edu.br>
2026-04-10 08:53:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Simples </title>
+</head>
+<body>
+    <h1>Enviar Arquivo</h1>
+
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="arquivo">
+    <button type="submit">Enviar</button>
+</form>
+</body>
+</html>
