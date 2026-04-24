95a85739eed3f37798936b959bdea239a3f3a094
Julia <julia.04910@aluno.iffar.edu.br>
2026-04-24 09:30:51 -0300

    Add files via upload

--- aula2.js
+++ aula2.js
@@ -16,7 +16,19 @@ let nomecompleto = "giulia Moccelin"
 console.log($valor5)
 console.log(nomeCompleto)
 console.log(nomecompleto)
-console.log(`meu nome é ${nome} e eu tenho ${idade} anos`)
 //crase dentro de aspas não funciona
+console.log(`meu nome é ${nome} e eu tenho ${idade} anos`)
+
+//inserir a idade  no incio e dps imprime
 const idade3=prompt("Qual é a sua idade?")
 console.log(`Você tem ${idade3} anos`)
+//alerta
+alert("error")
+//pega o maior valor entre os números
+console.log(Math.max(26,76,30, 42, 58))
+//pega o menor valor entre os números
+console.log(Math.min(26,76,30, 42, 58))
+//arredonda para o numero inteiro mais proximo
+console.log(Math.floor(5.32))
+//raiz do numero
+console.log(Math.sqrt(81))
