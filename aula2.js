65a0ccf39bd1eb27eb9f85ec8c42c7edd4f132e2
Julia <julia.04910@aluno.iffar.edu.br>
2026-04-24 09:17:15 -0300

    Add files via upload

---
+++ aula2.js
@@ -0,0 +1,22 @@
+let nome = "Julia"
+let idade = 17
+const valor1 = 16
+console.log(nome)
+console.log(idade)
+console.log(typeof valor1)
+console.log(typeof nome)
+let a = 15, b=36, v=76;
+//let 12valor=13
+//const @valor3 = 15
+let $valor5 =14
+let _nome="Maria"
+console.log(a)
+let nomeCompleto = "julia Amaral"
+let nomecompleto = "giulia Moccelin"
+console.log($valor5)
+console.log(nomeCompleto)
+console.log(nomecompleto)
+console.log(`meu nome é ${nome} e eu tenho ${idade} anos`)
+//crase dentro de aspas não funciona
+const idade3=prompt("Qual é a sua idade?")
+console.log(`Você tem ${idade3} anos`)
